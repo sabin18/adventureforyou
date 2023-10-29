430210485737ba646b56179a9e1d5bce1c28364d
sabin18 <sabin6341@gmail.com>
2023-10-29 23:03:34 +0200

    deploy

---
+++ src/data/aboutOne.js
@@ -0,0 +1,22 @@
+import about from "@/images/resources/adev_image.gif";
+import shape1 from "@/images/shapes/about-one-shape-1.png";
+import shape2 from "@/images/shapes/about-one-shape-2.png";
+
+const aboutOne = {
+  about,
+  shape1,
+  shape2,
+  phone: "+250788657132/+250780763545",
+  discount: "",
+  tagline: "Get to know us",
+  title: "Plan Your Trip with Adventure for you",
+  destination:
+    "There are many variations of passages of available but the majority have suffered alteration in some form, by injected hum randomised words which don't look even slightly.",
+  points: [
+    "Invest in your simply neighborhood",
+    "Support people in free text extreme need",
+    "Largest global industrial business community",
+  ],
+};
+
+export default aboutOne;
